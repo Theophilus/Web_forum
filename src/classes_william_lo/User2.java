b14c15346d20969b64aa60b82394f5692d57efef
lowill <lowill@eden.rutgers.edu>
2014-04-06 20:26:27 -0400

    basic pages

---
+++ src/classes_william_lo/User2.java
@@ -0,0 +1,63 @@
+package classes_william_lo;
+
+public class User2 {
+	String username;
+	String password;
+	String email;
+	String fname;
+	String lname;
+	String company;
+	String address;
+	String telephone;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	
+}
